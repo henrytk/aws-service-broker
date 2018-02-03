f7562a1fffd50bb1c340f6ef78edadc0bbf67065
henrytk <henrytknott@gmail.com>
2018-02-03 12:48:28 +0000

    wip

--- integration_tests/aws/cloudformation/mongodb/mongodb_suite_test.go
+++ integration_tests/aws/cloudformation/mongodb/mongodb_suite_test.go
@@ -27,6 +27,11 @@ var (
 	secondary1NodeSubnetId string
 	mongoDBAdminPassword   string
 	vpcId                  string
+	internetGatewayId      *string
+	routeTableId           *string
+	association1           *string
+	association2           *string
+	association3           *string
 	bastionSecurityGroupId string
 )
 
@@ -38,7 +43,8 @@ func TestMongodb(t *testing.T) {
 		secondary0NodeSubnetId = CreateSubnet("10.0.1.0/24")
 		secondary1NodeSubnetId = CreateSubnet("10.0.2.0/24")
 		bastionSecurityGroupId = CreateSecurityGroup("bastion-mongodb-test")
-		mongoDBAdminPassword = "volunteer pilot"
+		CreateRouteTable()
+		mongoDBAdminPassword = "volunteer-pilot"
 
 		mongoDBService, err = mongodb.NewService(region)
 		Expect(err).NotTo(HaveOccurred())
@@ -46,6 +52,7 @@ func TestMongodb(t *testing.T) {
 	})
 
 	AfterSuite(func() {
+		DeleteRouteTable()
 		DeleteSecurityGroup(bastionSecurityGroupId)
 		DeleteSubnet(secondary0NodeSubnetId)
 		DeleteSubnet(secondary1NodeSubnetId)
@@ -68,11 +75,40 @@ func CreateVPC() {
 		CidrBlock: aws.String("10.0.0.0/16"),
 	})
 	Expect(err).NotTo(HaveOccurred())
-	vpcId = *vpcOutput.Vpc.VpcId
+	awsVpcId := vpcOutput.Vpc.VpcId
+
+	createInternetGatewayOutput, err := ec2Service.CreateInternetGateway(
+		&ec2.CreateInternetGatewayInput{},
+	)
+	Expect(err).NotTo(HaveOccurred())
+
+	internetGatewayId = createInternetGatewayOutput.InternetGateway.InternetGatewayId
+	_, err = ec2Service.AttachInternetGateway(
+		&ec2.AttachInternetGatewayInput{
+			InternetGatewayId: internetGatewayId,
+			VpcId:             awsVpcId,
+		},
+	)
+	Expect(err).NotTo(HaveOccurred())
+
+	vpcId = *awsVpcId
 }
 
 func DeleteVPC() {
-	_, err := ec2Service.DeleteVpc(&ec2.DeleteVpcInput{
+	_, err := ec2Service.DetachInternetGateway(
+		&ec2.DetachInternetGatewayInput{
+			InternetGatewayId: internetGatewayId,
+			VpcId:             aws.String(vpcId),
+		},
+	)
+	Expect(err).NotTo(HaveOccurred())
+
+	_, err = ec2Service.DeleteInternetGateway(&ec2.DeleteInternetGatewayInput{
+		InternetGatewayId: internetGatewayId,
+	})
+	Expect(err).NotTo(HaveOccurred())
+
+	_, err = ec2Service.DeleteVpc(&ec2.DeleteVpcInput{
 		VpcId: aws.String(vpcId),
 	})
 	Expect(err).NotTo(HaveOccurred())
@@ -90,6 +126,66 @@ func CreateSubnet(cidrBlock string) string {
 func DeleteSubnet(subnetId string) {
 	_, err := ec2Service.DeleteSubnet(&ec2.DeleteSubnetInput{
 		SubnetId: aws.String(subnetId),
+	})
+	Expect(err).NotTo(HaveOccurred())
+}
+
+func CreateRouteTable() {
+	createRouteTableOutput, err := ec2Service.CreateRouteTable(&ec2.CreateRouteTableInput{
+		VpcId: aws.String(vpcId),
+	})
+	Expect(err).NotTo(HaveOccurred())
+
+	routeTableId = createRouteTableOutput.RouteTable.RouteTableId
+
+	_, err = ec2Service.CreateRoute(&ec2.CreateRouteInput{
+		DestinationCidrBlock: aws.String("0.0.0.0/0"),
+		GatewayId:            internetGatewayId,
+		RouteTableId:         routeTableId,
+	})
+	Expect(err).NotTo(HaveOccurred())
+
+	associateRouteTableOutput1, err := ec2Service.AssociateRouteTable(&ec2.AssociateRouteTableInput{
+		RouteTableId: routeTableId,
+		SubnetId:     aws.String(primaryNodeSubnetId),
+	})
+	Expect(err).NotTo(HaveOccurred())
+	association1 = associateRouteTableOutput1.AssociationId
+
+	associateRouteTableOutput2, err := ec2Service.AssociateRouteTable(&ec2.AssociateRouteTableInput{
+		RouteTableId: routeTableId,
+		SubnetId:     aws.String(secondary0NodeSubnetId),
+	})
+	Expect(err).NotTo(HaveOccurred())
+	association2 = associateRouteTableOutput2.AssociationId
+
+	associateRouteTableOutput3, err := ec2Service.AssociateRouteTable(&ec2.AssociateRouteTableInput{
+		RouteTableId: routeTableId,
+		SubnetId:     aws.String(secondary1NodeSubnetId),
+	})
+	Expect(err).NotTo(HaveOccurred())
+	association3 = associateRouteTableOutput3.AssociationId
+}
+
+func DeleteRouteTable() {
+	_, err := ec2Service.DisassociateRouteTable(&ec2.DisassociateRouteTableInput{
+		AssociationId: association3,
+	})
+	Expect(err).NotTo(HaveOccurred())
+
+	_, err = ec2Service.DisassociateRouteTable(&ec2.DisassociateRouteTableInput{
+		AssociationId: association2,
+	})
+	Expect(err).NotTo(HaveOccurred())
+
+	_, err = ec2Service.DisassociateRouteTable(&ec2.DisassociateRouteTableInput{
+		AssociationId: association1,
+	})
+	Expect(err).NotTo(HaveOccurred())
+
+	Expect(err).NotTo(HaveOccurred())
+	_, err = ec2Service.DeleteRouteTable(&ec2.DeleteRouteTableInput{
+		RouteTableId: routeTableId,
 	})
 	Expect(err).NotTo(HaveOccurred())
 }
